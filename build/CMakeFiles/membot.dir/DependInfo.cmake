7dfeef93602362668560c4ed6104eaef1ad9950d
Jed Helmers <jedhelmers@gmail.com>
2020-10-14 21:53:35 -0400

    first commit

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/src/chatbot.cpp" "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/src/chatgui.cpp" "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/src/chatlogic.cpp" "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/src/graphedge.cpp" "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/src/graphnode.cpp" "/Users/jed/Documents/Development/Udacity/CPP/CppND-Memory-Management-Chatbot_ALT/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "_FILE_OFFSET_BITS=64"
+  "__WXMAC__"
+  "__WXOSX_COCOA__"
+  "__WXOSX__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/osx_cocoa-unicode-3.0"
+  "/usr/local/include/wx-3.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
